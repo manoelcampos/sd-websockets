f41905a993936cb31a653c5d03d514399bbadf3f
Manoel Campos <manoelcampos@gmail.com>
2022-09-12 17:50:40 -0300

    Refatora projeto chat websockets

--- 2.1-websocket-chat-nodejs/server.js
+++ 2.1-websocket-chat-nodejs/server.js
@@ -7,20 +7,21 @@ app.use(express.static("public"))
 // https://nodejs.org/api/http.html
 const http = require('http').Server(app)
 
+// https://socket.io
 const serverSocket = require('socket.io')(http)
 
-const porta = process.env.PORT || 8000
+const PORTA = process.env.PORT || 8000
 
 const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
 
-http.listen(porta, () => {
-    const portaStr = porta === 80 ? '' :  ':' + porta
+http.listen(PORTA, () => {
+    const portaStr = PORTA === 80 ? '' :  ':' + PORTA
     if (process.env.HEROKU_APP_NAME)
         console.log('Servidor iniciado. Abra o navegador em ' + host)
     else console.log('Servidor iniciado. Abra o navegador em ' + host + portaStr)
 })
 
-app.get('/', (requisicao, resposta) => resposta.sendFile(__dirname + '/index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 serverSocket.on('connect', recebeConexaoUsuario)
 
